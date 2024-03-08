5ee2cea8b33fe269cdbbd3a5387d1461c1068287
fraugho <aghoanebom@gmail.com>
2024-03-08 10:26:42 -0900

    Added File Tree

--- init.lua
+++ init.lua
@@ -1,3 +1,28 @@
 vim.opt.clipboard:append("unnamedplus")
 require("shadow.remap")
 require("shadow.set")
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
